de5f3ee1fdb9147cc0da87f3e8f4f8f9852741c2
ginsum-dev <jennyjung.dev@gmail.com>
2025-06-28 00:43:18 +0900

    Feat: Main Title 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/ui/Header";
+import Title from "@/components/ui/sections/Title";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen">
       <Header />
-      <main className="flex flex-col p-8 pb-20 gap-16">
-        <h1>Hello World</h1>
+      <main className="flex flex-col p-8 pb-20 gap-16 min-h-[calc(100vh-100px)]">
+        <Title />
       </main>
       <footer className="flex gap-[24px] flex-wrap items-center justify-center">
         <p>Footer</p>
